bea68abefac8552824729c7567e7591a28cc9226
Allen Preet Singh <allenpreetsingh@Allens-MacBook-Air.local>
2025-04-15 14:03:00 +0530

    learning 2 is working

---
+++ src/main/java/com/learning_git/learning_git/LearningGitApplication.java
@@ -0,0 +1,13 @@
+package com.learning_git.learning_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning_git/learning_git/LearningGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning_git.learning_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
